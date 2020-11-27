8ed32ecdbb2efc6f7128e6dc077a42f9a9960e5f
mouelhimariem <mouelhimariem63@yahoo.fr>
2020-11-27 14:41:13 +0100

    Ajout du composant materie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListmaterielComponent } from './informatique/listmateriel/listmateriel.component';
+import { DetailmaterielComponent } from './informatique/detailmateriel/detailmateriel.component';
+import { MaterielComponent } from './informatique/materiel/materiel.component';
+import { CommentComponent } from './commentaires/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListmaterielComponent,
+    DetailmaterielComponent,
+    MaterielComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/informatique/detailmateriel/detailmateriel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detailmateriel',
+  templateUrl: './detailmateriel.component.html',
+  styleUrls: ['./detailmateriel.component.css']
+})
+export class DetailmaterielComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
